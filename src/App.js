cacc4a25a6a810161c1939d22807bf2f7693bf0e
julinha13 <ju.amorim13@gmail.com>
2020-12-23 18:54:40 -0300

    initializing project, defining global theme, uploading logos and thumbs

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core';
+
+import HomePage from './HomePage'
+
+function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const theme = createMuiTheme({
+    palette: {
+      type: darkMode ? 'dark' : 'light',
+      primary: {
+        main: '#f44336',
+      },
+      secondary: {
+        main: '#3ea6FF',
+      },
+      background: {
+        default: darkMode ? '#232323' : '#FFF',
+        dark: darkMode ? '#181818' : '#f4f6f8',
+        paper: darkMode ? '#232323' : '#FFF'
+      }
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <HomePage darkMode={darkMode} setDarkMode={setDarkMode}/>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/HomePage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function HomePage() {
+   return (
+       <p>Hello World</p>
+   );
+}
+export default HomePage;
